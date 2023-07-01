0c7bcb8826413fd905419137d1ac64ff4d178dc1
manjozib <manjozib@gmail.com>
2023-07-01 20:40:10 +0200

    project set up

---
+++ src/components/Home/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Home() {
+    return(
+        <div className="bg-gray-100 h-screen flex items-center justify-center">
+            <h1 className="text-3xl font-bold text-green-500">Hello, Tailwind CSS!</h1>
+        </div>
+    )
+}
